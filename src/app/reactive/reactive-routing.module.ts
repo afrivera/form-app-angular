fb431da2940432b41033b3150a223bdd74c07c94
afrivera <andresrfelip@hotmail.com>
2022-06-06 16:45:14 -0500

    create components and routes templates and reactive

--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BasicsComponent } from './pages/basics/basics.component';
+import { DinamicsComponent } from './pages/dinamics/dinamics.component';
+import { SwitchsComponent } from './pages/switchs/switchs.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'basics', component: BasicsComponent },
+      { path: 'dinamics', component: DinamicsComponent },
+      { path: 'switchs', component: SwitchsComponent },
+      { path: '**', redirectTo: 'basics'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TemplateRoutingModule } from './template-routing.module';
+import { BasicsComponent } from './pages/basics/basics.component';
+import { DinamicsComponent } from './pages/dinamics/dinamics.component';
+import { SwitchsComponent } from './pages/switchs/switchs.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicsComponent,
+    DinamicsComponent,
+    SwitchsComponent
+  ],
   imports: [
     CommonModule,
     TemplateRoutingModule
