057764c01dc083e898bcd92b7b6da45f43012dbe
afrivera <andresrfelip@hotmail.com>
2022-06-11 16:56:05 -0500

    config switch checks reactive form

--- src/app/reactive/pages/switchs/switchs.component.ts
+++ src/app/reactive/pages/switchs/switchs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switchs',
@@ -8,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchsComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required ],
+    notifications: [ true, Validators.required ],
+    conditions: [ false, Validators.requiredTrue ],
+  })
+
+  person = {
+    gender: 'F',
+    notifications: true
+  }
+
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myForm.reset( {
+      ...this.person,
+      conditions: false
+    } );
+
+    // this.myForm.get('conditions')?.valueChanges.subscribe( newValue => {
+    //   console.log(newValue);
+    // });
+
+    this.myForm.valueChanges.subscribe( ({conditions, ...form}) => {
+      // delete form.conditions;
+      // console.log(form);
+      this.person = form;
+
+    })
   }
+
+  save(){
+    const formValue = {...this.myForm.value};
+    delete formValue.conditions
+    // console.log(formValue);
+    this.person = formValue;
+
+  }
+
 
 }
