6abfb6035e606b371f3fcaeb6385d1e2e476173d
afrivera <andresrfelip@hotmail.com>
2022-06-12 15:37:22 -0500

    config validations with regex

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
+      { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
-      { path: 'login', component: LoginComponent }
+      { path: '**', redirectTo: 'register' }
     ]
   }
 ];
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // TODO: temporal
+  namePattern : string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}";
+
+
+  myForm: FormGroup = this.fb.group({
+    name: [ , [ Validators.required, Validators.pattern( this.namePattern ) ]],
+    email: [ , [ Validators.required, Validators.pattern( this.emailPattern )]]
+  })
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.myForm.reset({
+      name: 'Andres Felipe',
+      email: 'test@test.com'
+    })
+  }
+
+  invalidField( field: string ){
+    return this.myForm.get( field )?.invalid
+            && this.myForm.get( field )?.touched
+  }
+
+  saveForm(){
+    console.log( this.myForm.value );
+    this.myForm.markAllAsTouched();
   }
 
 }
